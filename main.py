a64cc42b471d48e3d481bac2870a9cce23e4397b
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-25 14:27:30 +0200

    - added config files - imported font

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ screen = pygame.display.set_mode((500, 600))
 pygame.display.set_caption("sudoku solver")
 #img = pygame.image.load('icon.png')
 #pygame.display.set_icon(img)
+font = pygame.font.SysFont("calibri", 20)
+
+# empty sudoku
+grid = np.zeros((9, 9))
