af6d96bbb100727448a65e4f726f777f16f2670e
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-25 14:22:57 +0200

    - pygame window config

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import numpy as np
 
 
 pygame.font.init()
+screen = pygame.display.set_mode((500, 600))
+
+pygame.display.set_caption("sudoku solver")
+#img = pygame.image.load('icon.png')
+#pygame.display.set_icon(img)
