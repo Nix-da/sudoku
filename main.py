9ec2c3053c83a03649a076439eb07a5feae25dc4
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-25 15:21:43 +0200

    - parameterized

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+import math
+
 import pygame
 import numpy as np
 
 
+sudoku_segment_count = 3
+sudoku_grid_count = sudoku_segment_count ** 2
+sudoku_cell_size = 50
+sudoku_cell_line = 1
+sudoku_cell_border = 5
+sudoku_cell_highlight = 3
+sudoku_offset = 40
+
+window_width = sudoku_grid_count * sudoku_cell_size + 2 * sudoku_offset + 6 * sudoku_cell_line + 3 * sudoku_cell_border
+window_height = 1.1 * sudoku_grid_count * sudoku_cell_size + 2 * sudoku_offset + 6 * sudoku_cell_line + 3 * sudoku_cell_border
+
+# empty sudoku
+grid = np.zeros((sudoku_grid_count, sudoku_grid_count))
+
 pygame.font.init()
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((window_width, window_height))
 
 pygame.display.set_caption("sudoku solver")
 #img = pygame.image.load('icon.png')
 #pygame.display.set_icon(img)
 font = pygame.font.SysFont("calibri", 20)
 
-# empty sudoku
-grid = np.zeros((9, 9))
-
 
 def draw():
-    dif = 500 / 9
-    for i in range(9):
-        for j in range(9):
+    for i in range(sudoku_grid_count):
+        for j in range(sudoku_grid_count):
             if grid[i][j] != 0:
-                pygame.draw.rect(screen, (0, 153, 153), (i * dif, j * dif, dif + 1, dif + 1))
+                pygame.draw.rect(
+                    screen,
+                    (0, 153, 153),
+                    (i * sudoku_cell_size, j * sudoku_cell_size, sudoku_cell_size + 1, sudoku_cell_size + 1)
+                )
 
-    for i in range(10):
-        if i % 3 == 0:
-            thick = 7
+    for i in range(sudoku_grid_count + 1):
+        if i % sudoku_segment_count == 0:
+            line = sudoku_cell_border
         else:
-            thick = 1
-        pygame.draw.line(screen, (5, 5, 5), (5, i * dif), (500, i * dif), thick)
-        pygame.draw.line(screen, (0, 0, 0), (i * dif, 0), (i * dif, 500), thick)
+            line = sudoku_cell_line
+        pygame.draw.line(screen, (5, 5, 5), (5, i * sudoku_cell_size), (window_width, i * sudoku_cell_size), line)
+        pygame.draw.line(screen, (0, 0, 0), (i * sudoku_cell_size, 0), (i * sudoku_cell_size, window_width), line)
 
 
 run = True
