9d743aad473c460d5d92f522a16df63a3b3a9e8e
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-26 10:04:58 +0200

    - draw rectangles

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ sudoku_cell_border = 5
 sudoku_cell_highlight = 3
 sudoku_offset = 40
 
-window_width = sudoku_grid_count * sudoku_cell_size + 2 * sudoku_offset + 6 * sudoku_cell_line + 3 * sudoku_cell_border
-window_height = 1.1 * sudoku_grid_count * sudoku_cell_size + 2 * sudoku_offset + 6 * sudoku_cell_line + 3 * sudoku_cell_border
+sudoku_width = sudoku_grid_count * sudoku_cell_size + 6 * sudoku_cell_line + 7 * sudoku_cell_border
+sudoku_height = sudoku_grid_count * sudoku_cell_size + 6 * sudoku_cell_line + 7 * sudoku_cell_border
+
+window_width = sudoku_width + 2 * sudoku_offset
+window_height = 1.1 * sudoku_height + 2 * sudoku_offset
 
 # empty sudoku
-grid = np.zeros((sudoku_grid_count, sudoku_grid_count))
+grid = np.ones((sudoku_grid_count, sudoku_grid_count))
 
 pygame.font.init()
 screen = pygame.display.set_mode((window_width, window_height))
@@ -33,17 +36,12 @@ def draw():
             if grid[i][j] != 0:
                 pygame.draw.rect(
                     screen,
-                    (0, 153, 153),
-                    (i * sudoku_cell_size, j * sudoku_cell_size, sudoku_cell_size + 1, sudoku_cell_size + 1)
+                    (0, 0, 0),
+                    (i * sudoku_cell_size, j * sudoku_cell_size, sudoku_cell_size + 1, sudoku_cell_size + 1),
+                    1
                 )
-
-    for i in range(sudoku_grid_count + 1):
-        if i % sudoku_segment_count == 0:
-            line = sudoku_cell_border
-        else:
-            line = sudoku_cell_line
-        pygame.draw.line(screen, (5, 5, 5), (5, i * sudoku_cell_size), (window_width, i * sudoku_cell_size), line)
-        pygame.draw.line(screen, (0, 0, 0), (i * sudoku_cell_size, 0), (i * sudoku_cell_size, window_width), line)
+                #text1 = font.render(str(grid[i][j]), 1, (0, 0, 0))
+                #screen.blit(text1, (i * sudoku_cell_size + sudoku_offset, j * sudoku_cell_size + sudoku_offset))
 
 
 run = True
