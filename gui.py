8ad4c9e7249ed9b129433342b9f12e5a5ffeac1a
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-27 14:52:42 +0200

    - stole colors from sudoku.com (muahaha)

--- gui.py
+++ gui.py
@@ -6,6 +6,15 @@ class Sudoku_user_interface():
     sudoku_cell_border = 2
     sudoku_offset = 40
 
+    border_color = (52, 72, 97)
+    line_color = (191, 198, 212)
+    original_color = (52, 72, 97)
+    solutions_color = (50, 90, 175)
+    possibilities_color = (110, 124, 140)
+    selected_bckgr_color = (187, 222, 251)
+    observation_bckgr_color = (226, 235, 243)
+
+
     def __init__(self, sudoku):
         self.sudoku = sudoku
         self.sudoku_width = self.sudoku.grid_count * self.sudoku_cell_size + \
@@ -25,27 +34,33 @@ class Sudoku_user_interface():
         pygame.display.set_caption("sudoku solver")
         # img = pygame.image.load('icon.png')
         # pygame.display.set_icon(img)
-        self.font = pygame.font.SysFont("calibri", 35)
+        self.font_original = pygame.font.SysFont("calibri", 35)
+        self.font_solution = pygame.font.SysFont("calibri", 35)
+        self.font_selected = pygame.font.SysFont("calibri", 35, bold=True)
+        self.font_observed = pygame.font.SysFont("calibri", 35)
+        self.font_possible = pygame.font.SysFont("calibri", 7)
 
     def quit(self):
         pygame.quit()
 
     def draw(self):
-        # draw border
-        start_coord = (
-            self.sudoku_offset,
-            self.sudoku_offset
-        )
-        dimensions = (
-            self.sudoku_cell_size * self.sudoku.grid_count,
-            self.sudoku_cell_size * self.sudoku.grid_count
-        )
-        pygame.draw.rect(
-            self.screen,
-            (0, 0, 0),
-            (start_coord, dimensions),
-            self.sudoku_cell_border * 2
-        )
+        # draw small cells
+        for i in range(self.sudoku.grid_count):
+            for j in range(self.sudoku.grid_count):
+                start_coord = (
+                    self.sudoku_offset + i * self.sudoku_cell_size,
+                    self.sudoku_offset + j * self.sudoku_cell_size
+                )
+                dimensions = (
+                    self.sudoku_cell_size,
+                    self.sudoku_cell_size
+                )
+                pygame.draw.rect(
+                    self.screen,
+                    self.line_color,
+                    (start_coord, dimensions),
+                    1
+                )
 
         # draw segments
         for i in range(self.sudoku.segment_count):
@@ -60,34 +75,39 @@ class Sudoku_user_interface():
                 )
                 pygame.draw.rect(
                     self.screen,
-                    (0, 0, 0),
+                    self.border_color,
                     (start_coord, dimensions),
                     self.sudoku_cell_border
                 )
 
-        # draw small cells
-        for i in range(self.sudoku.grid_count):
-            for j in range(self.sudoku.grid_count):
-                start_coord = (
-                    self.sudoku_offset + i * self.sudoku_cell_size,
-                    self.sudoku_offset + j * self.sudoku_cell_size
-                )
-                dimensions = (
-                    self.sudoku_cell_size,
-                    self.sudoku_cell_size
-                )
-                pygame.draw.rect(
-                    self.screen,
-                    (0, 0, 0),
-                    (start_coord, dimensions),
-                    1
-                )
+        # draw border
+        start_coord = (
+            self.sudoku_offset,
+            self.sudoku_offset
+        )
+        dimensions = (
+            self.sudoku_cell_size * self.sudoku.grid_count,
+            self.sudoku_cell_size * self.sudoku.grid_count
+        )
+        pygame.draw.rect(
+            self.screen,
+            self.border_color,
+            (start_coord, dimensions),
+            self.sudoku_cell_border * 2
+        )
 
         # write numbers
         for i in range(self.sudoku.grid_count):
             for j in range(self.sudoku.grid_count):
                 if not self.sudoku.original_sudoku[i][j] == 0:
-                    text = self.font.render(str(self.sudoku.original_sudoku[i][j]), 1, (0, 0, 0))
+                    text = self.font_original.render(str(self.sudoku.original_sudoku[i][j]), 1, self.original_color)
+                    self.screen.blit(
+                        text,
+                        (i * self.sudoku_cell_size + self.sudoku_offset + 16,
+                         j * self.sudoku_cell_size + self.sudoku_offset + 9)
+                    )
+                if not self.sudoku.solutions_sudoku[i][j] == 0:
+                    text = self.font_solution.render(str(self.sudoku.original_sudoku[i][j]), 1, self.solutions_color)
                     self.screen.blit(
                         text,
                         (i * self.sudoku_cell_size + self.sudoku_offset + 16,
