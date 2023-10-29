2c30c6a3621f6c12df1ae8d7add155d44b4d9905
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-10-29 17:27:12 +0100

    - added SudokuSolver Class

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from sudoku import Sudoku
+from sudoku_solver import SudokuSolver
 from gui import Sudoku_user_interface
 
 sudoku = Sudoku()
+sudoku_solver = SudokuSolver(sudoku)
 gui = Sudoku_user_interface(sudoku)
 gui.open()
 
 run = True
 
 while run:
-    run = gui.update()
+    event = gui.update()
+    if event == "Quit":
+        run = False
+
+    run = sudoku_solver.solve(event)
 
 gui.quit()
