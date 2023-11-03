2121889972eec2d00629741e367c2cf17f845725
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-11-03 15:30:32 +0100

    - removed unused code

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -40,21 +40,6 @@ class SudokuSolver:
         else:
             self.sudoku.observations = []
 
-    def scan_field(self):
-        if self.sudoku.get_selected_coord() is None:
-            self.sudoku.select((0, 0))
-        else:
-            current_coord = self.sudoku.get_selected_coord()
-            next_position = (1 + self.sudoku.index_from_coord(current_coord)) % self.sudoku.get_grid_count() ** 2
-            self.sudoku.select(next_position)
-        if not self.sudoku.get_selected_number() == 0:
-            self.sudoku.observations = self.find_all_numbers(self.sudoku.get_selected_number())
-            self.sudoku.observations += self.sudoku.get_row(self.sudoku.get_selected_coord())[1]
-            self.sudoku.observations += self.sudoku.get_col(self.sudoku.get_selected_coord())[1]
-            self.sudoku.observations += self.sudoku.get_segment(self.sudoku.get_selected_coord())[1]
-        else:
-            self.sudoku.observations = []
-
     def fill_possibilities(self):
         if self.sudoku.get_number(self.sudoku.get_selected_coord()) == 0:
 
