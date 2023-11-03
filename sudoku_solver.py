b7ef70459466d1d913e45522f1e47f395c19fbc3
Nix_da <nick.geisler@mailbox.tu-dresden.de>
2023-11-03 14:31:49 +0100

    - exported event function to seperate function

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -13,21 +13,33 @@ class SudokuSolver:
             coords.append((findings[0][i], findings[1][i]))
         return coords
 
+    def scan_field(self):
+        if self.sudoku.get_selected_coord() is None:
+            self.sudoku.select((0, 0))
+        else:
+            current_coord = self.sudoku.get_selected_coord()
+            next_position = (1 + self.sudoku.index_from_coord(current_coord)) % self.sudoku.get_grid_count() ** 2
+            self.sudoku.select(next_position)
+        if not self.sudoku.get_selected_number() == 0:
+            self.sudoku.observations = self.find_all_numbers(self.sudoku.get_selected_number())
+            self.sudoku.observations += (self.sudoku.get_row(self.sudoku.get_selected_coord())[1])
+            self.sudoku.observations += (self.sudoku.get_col(self.sudoku.get_selected_coord())[1])
+            self.sudoku.observations += (self.sudoku.get_segment(self.sudoku.get_selected_coord())[1])
+        else:
+            self.sudoku.observations = []
+
+    def fill_possibilities(self):
+        if self.sudoku.get_number(self.sudoku.get_selected_coord()) == 0:
+            self.sudoku.set_possibilities(self.sudoku.get_selected_coord(), [1,2,3,4,5,6,7,8,9])
+
     def solve(self, action):
         if action == "nothing":
             return True
         if action == "scan field":
-            if self.sudoku.get_selected_coord() is None:
-                self.sudoku.select((0, 0))
-            else:
-                current_coord = self.sudoku.get_selected_coord()
-                next_position = (1 + self.sudoku.index_from_coord(current_coord)) % self.sudoku.get_grid_count() ** 2
-                self.sudoku.select(next_position)
-            if not self.sudoku.get_selected_number() == 0:
-                self.sudoku.observations = self.find_all_numbers(self.sudoku.get_selected_number())
-            else:
-                self.sudoku.observations = []
-                # self.sudoku.selected_number = 0
+            self.scan_field()
+            return True
+        if action == "fill possibilities":
+            self.fill_possibilities()
             return True
 
         return False
